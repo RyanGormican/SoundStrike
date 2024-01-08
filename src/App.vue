21c68035c6bdd7ccf4b824b6bc9987a32788b9ec
RyanGormican <ryan.gormican@gmail.com>
2024-01-08 04:23:32 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <div class="carousel-container">
-      <div class="carousel-arrow left" @click="moveCarousel('left')">&#8249;</div>
-      <div class="carousel" :style="{ transform: `translateX(${-currentIndex * 50}%)` }">
-        <div v-for="(item, index) in visibleItems" :key="index" class="carousel-item">
-          <h3>{{ item.title }}</h3>
-          <p>{{ item.description }}</p>
+    <div class="container">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div v-for="(item, index) in items" :key="index" :class="{ 'carousel-item': true, active: index === 0 }">
+            <div class="row">
+              <div v-for="(letter, letterIndex) in ['a', 'b', 'c', 'd']" :key="letterIndex" class="col-md-6">
+                <h3>{{ items[(index * 4 + letterIndex) % items.length].title }}</h3>
+                <p>{{ items[(index * 4 + letterIndex) % items.length].description }}</p>
+              </div>
+            </div>
+          </div>
         </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <div class="carousel-arrow right" @click="moveCarousel('right')">&#8250;</div>
     </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 export default {
   name: 'App',
   data() {
@@ -28,75 +43,17 @@ export default {
         { title: 'Item 7', description: 'Description 7' },
         { title: 'Item 8', description: 'Description 8' },
       ],
-      currentIndex: 0,
-      itemsPerPage: 4,
     };
   },
-  computed: {
-    visibleItems() {
-      const start = this.currentIndex;
-      const end = start + this.itemsPerPage;
-      return this.items.slice(start, end);
-    },
-  },
-methods: {
-  moveCarousel(direction) {
-    const totalItems = this.items.length;
-
-    if (direction === 'left') {
-      this.currentIndex = (this.currentIndex - 1 + totalItems) % totalItems;
-    } else if (direction === 'right') {
-      this.currentIndex = (this.currentIndex + 1) % totalItems;
-    }
-  },
-},
 };
 </script>
 
 <style>
 #app {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  overflow: hidden; 
+  overflow: hidden;
+  background-color: lightblue;
+  min-height:100vh;
 }
 
-.carousel-container {
-  display: flex;
-  position: relative;
-  height: 100vh;
-  width: 100vw;
-}
 
-.carousel {
-  display: flex;
-  width: 200%; 
-  transition: transform 0.5s ease; 
-}
-
-.carousel-item {
-  flex: 0 0 50%; 
-  box-sizing: border-box;
-  padding: 10px;
-  text-align: center;
-  border: 1px solid #ddd;
-}
-
-.carousel-arrow {
-  cursor: pointer;
-  font-size: 24px;
-  margin: 0 10px;
-  position: absolute;
-  top: 50%;
-  color: #333;
-}
-
-.left {
-  left: 0;
-}
-
-.right {
-  right: 0;
-}
 </style>
